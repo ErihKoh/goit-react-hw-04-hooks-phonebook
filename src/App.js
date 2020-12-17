947aed308a94642189ff36e9652debe82a09a975
Yurii Zubar <barsuk0831@gmail.com>
2020-12-17 19:10:42 +0200

    completed Contacts form

--- src/App.js
+++ src/App.js
@@ -1,114 +1,12 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/PNotify.css';
-import { error, notice } from '@pnotify/core';
-import { defaults } from '@pnotify/core';
 import './App.css';
-import Section from './components/Section';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
-// import contacts from './components/ContactList/contacts';
+import PhonebookView from './components/PhonebookView';
 
-defaults.delay = 2000;
-
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  static propTypes = {
-    contacts: PropTypes.array,
-    filter: PropTypes.string,
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parseContacts = JSON.parse(contacts);
-    parseContacts && this.setState({ contacts: parseContacts });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const nextContacts = this.state.contacts;
-    const prevcontacts = prevState.contacts;
-
-    if (nextContacts !== prevcontacts) {
-      localStorage.setItem('contacts', JSON.stringify(nextContacts));
-    }
-  }
-
-  // функция для получения данных из формы и добавдение в контакты
-
-  addContact = data => {
-    const { contacts } = this.state;
-    if (data.name === '') {
-      return error({
-        title: 'Error',
-        text: 'Please eneter name!',
-      });
-    }
-    if (data.number === '') {
-      return error({
-        title: 'Error',
-        text: 'Please eneter number!',
-      });
-    }
-
-    const findContact = contacts.find(({ name }) => data.name === name);
-
-    if (!findContact) {
-      this.setState(({ contacts }) => ({
-        contacts: [data, ...contacts],
-      }));
-      return;
-    }
-
-    if (findContact.name === data.name) {
-      notice({
-        title: 'Notice',
-        text: `${data.name} is already in contacts.`,
-      });
-    }
-  };
-
-  // функция для удаления контактов
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  //  функция фильтрации контактов по имени
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  render() {
-    const normalizeFilter = this.state.filter.toLowerCase();
-    const { contacts } = this.state;
-    const filterContact = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizeFilter),
-    );
-
-    return (
-      <div className="block">
-        <Section title="Phonebook">
-          <ContactForm onSubmit={this.addContact} />
-        </Section>
-        <Section title="Contacts">
-          <Filter value={this.state.filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={filterContact}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <PhonebookView />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,64 +1,110 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import s from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  static propTypes = {
-    name: PropTypes.string,
-    number: PropTypes.number,
-  };
+export default function ContactForm(props) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleInputChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.onSubmit({ ...this.state, id: shortid.generate() });
-    this.reset();
+    props.onSubmit({ name, number, id: shortid.generate() });
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={s.form}>
-        <label className={s.name}>Name</label>
-        <input
-          className={s.inputName}
-          type="text"
-          name="name"
-          value={this.state.name}
-          autoComplete="off"
-          onChange={this.handleInputChange}
-        />
-        <label className={s.number}>Number</label>
-        <input
-          className={s.inputNumber}
-          type="tel"
-          name="number"
-          value={this.state.number}
-          autoComplete="off"
-          onChange={this.handleInputChange}
-        />
-        <button type="submit" className={s.btn}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit} className={s.form}>
+      <label className={s.name}>Name</label>
+      <input
+        className={s.inputName}
+        type="text"
+        name="name"
+        value={name}
+        autoComplete="off"
+        onChange={e => setName(e.target.value)}
+      />
+      <label className={s.number}>Number</label>
+      <input
+        className={s.inputNumber}
+        type="tel"
+        name="number"
+        value={number}
+        autoComplete="off"
+        onChange={e => setNumber(e.target.value)}
+      />
+      <button type="submit" className={s.btn}>
+        Add contact
+      </button>
+    </form>
+  );
 }
 
-export default ContactForm;
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+// static propTypes = {
+//   name: PropTypes.string,
+//   number: PropTypes.number,
+// };
+
+// handleInputChange = e => {
+//   const { name, value } = e.currentTarget;
+//   this.setState({ [name]: value });
+// };
+
+// handleSubmit = e => {
+//   e.preventDefault();
+
+//   this.props.onSubmit({ ...this.state, id: shortid.generate() });
+//   this.reset();
+// };
+
+// reset = () => {
+//   this.setState({
+//     name: '',
+//     number: '',
+//   });
+// };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className={s.form}>
+//         <label className={s.name}>Name</label>
+//         <input
+//           className={s.inputName}
+//           type="text"
+//           name="name"
+//           value={this.state.name}
+//           autoComplete="off"
+//           onChange={this.handleInputChange}
+//         />
+//         <label className={s.number}>Number</label>
+//         <input
+//           className={s.inputNumber}
+//           type="tel"
+//           name="number"
+//           value={this.state.number}
+//           autoComplete="off"
+//           onChange={this.handleInputChange}
+//         />
+//         <button type="submit" className={s.btn}>
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.number,
+};
